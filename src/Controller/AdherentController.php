21650a2239e48f24f301a65fea70056e585b7abd
alexandredieu <alex.dieudonne64@gmail.com>
2021-07-07 15:24:47 +0200

    save

--- src/Controller/AdherentController.php
+++ src/Controller/AdherentController.php
@@ -91,12 +91,4 @@ class AdherentController extends AbstractController
 
         return $this->redirectToRoute('adherent_index');
     }
-    /**
-     * @Route("/activite/add/{id}, name="activite_add)
-     */
-    // public function add($id, Request $request) {
-    //     $session = $request->getSession();
-    //     $user->addActivite();
-    // }
-
 }
